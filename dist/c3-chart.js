4bc4810c4bfe6f0d52d009d3de2f61bb6874d003
Mason <maseh87@gmail.com>
2014-11-11 15:48:59 -0800

    (feat) 0.0.2 complete

--- dist/c3-chart.js
+++ dist/c3-chart.js
@@ -15353,9 +15353,38 @@
 ;(function(c3){
   'use strict';
 
-angular.module('angular-c3', [])
+angular.module('ngC3', [])
+  .factory('c3Factory', function($q, $timeout) {
+    var defer = $q.defer();
+    var chart = {};
+    var allCharts = {};
+    var decorateChart = function(chart) {
+    };
 
-  .directive('c3Chart', [function() {
+    chart.get = function(id) {
+      var chart;
+      return $timeout(function() {
+        //time out to wait for the chart to be compiled
+      }, 100).then(function() {
+        chart = allCharts[id];
+        return chart;
+      });
+    };
+
+    chart.getAll = function() {
+      return $timeout(function() {
+        return allCharts;
+      }, 100);
+    };
+
+    chart.register = function(id, chart) {
+      decorateChart(chart);
+      allCharts[id] = chart;
+    };
+
+    return chart;
+  })
+  .directive('c3Chart', ['c3Factory', function(c3Factory) {
 
     //color patterns for chart coloring
     var patterns = {
@@ -15364,104 +15393,25 @@ angular.module('angular-c3', [])
       material: ['#e51c23', '#673ab7', '#5677fc', '#03a9f4', '#00bcd4', '#259b24', '#ffeb3b', '#ff9800']
     };
 
-    //random number to attach to the chart id
-    var chartIdCounter = Math.floor((Math.random()*1000)+1);
-
     return {
-      restrict: 'EA',
+      restrict: 'EAC',
       scope: {
-        data: '=',
-        options: '=',
-        axis: '='
+        config: '='
       },
-      template: '<div class="chart" style="height: 300px;"></div>',
+      template: '<div></div>',
       replace: true,
       link: function(scope, element, attrs) {
         //available option to show gridlines for chart
-        console.log(element);
-        if(attrs.grid === 'true') {
-          scope.grid = {
-            x: { show: true },
-            y: { show: true}
-          };
-        }
-        //option to view subchart
-        if(attrs.subchart === 'true') {
-          scope.subchart = {
-            show: true
-          };
-        }
-        //option to zoom in on chart
-        if(attrs.zoom === 'true') {
-          scope.zoom = { zoom: { enabled: true } };
-        }
-        //ability to change the color pattern
-        if(attrs.pattern) {
-          scope.color = {};
-          scope.color.pattern = patterns[attrs.pattern];
-        } else {
-          scope.color = {};
-          scope.color.pattern = patterns.dark ;
-        }
-        var chartId;
-        //if an id is given use that, if not assign it one
-        if(element.attr('id')) {
-          chartId = element.attr('id');
-        }
-        else {
-          chartId = 'c3-chart-' + chartIdCounter;
-          element.attr('id', chartId);
-          chartIdCounter += 1;
-        }
-        //click function not utilized currently
-        // scope.data.onclick = function(d, elem) {
-        //   console.log(elem.style.fill, ' elem');
-        //   elem.style.fill = '#ce93d8';
-        //   console.log(d, ' d');
-        // };
-        //generate c3 chart data
-        var chartData = {
-          bindto: '#' + element.attr('id'),
-          data: scope.data,
-          axis: scope.axis,
-          options: scope.options,
-          grid: scope.grid,
-          subchart: scope.subchart,
-          zoom: scope.zoom,
-          color: scope.color
-        };
         //assign a type of line if undefined
-        chartData.data.type = attrs.chart? attrs.chart : scope.data.type? scope.data.type : 'line';
+        if(!scope.config.type) scope.config.type = 'line';
 
-        //if scope.options are given replace the data with the options data
-        if(scope.options) {
-          Object.keys(scope.options).forEach(function(key) {
-            chartData[key] = scope.options[key];
-          });
-        }
-        //Reload the chart if the data changes
-        scope.$watch('data', function(data, prevData) {
-          if(chart) {
-            chart.load(data);
-            if(data.columns.length < prevData.columns.length) {
-              chart.unload(['data' + prevData.columns.length]);
-            }
-          }
-        });
-        //ran if there are changes to the chart
-        var onChartChanged = function(chart) {
-          if(chart) {
-            scope.data.type = chart;
-            chart.load(data);
-          }
-        };
-        //watch the chart for any changes
-        scope.$watch(function() {
-          return attrs.chart;
-        }, onChartChanged);
+        //generate c3 chart data
+        var chartData = scope.config;
+        chartData.bindto = '#' + attrs.id;
 
         //Generating the chart
         var chart = c3.generate(chartData);
+        c3Factory.register(attrs.id, chart);
       }
     };
   }]);
